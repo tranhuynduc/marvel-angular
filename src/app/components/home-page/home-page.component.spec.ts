8b8211452410eb83c46a1b4acd54e2a7b7393bb2
Tran Huynh Duc <tranhuynhduc.kt@gmail.com>
2019-04-12 14:18:48 +0700

    update test case for home page

--- src/app/components/home-page/home-page.component.spec.ts
+++ src/app/components/home-page/home-page.component.spec.ts
@@ -8,6 +8,7 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { MarvelService } from 'src/app/services/marvel.service';
 import { of } from 'rxjs';
 import { AppConfig } from 'src/app/configs';
+import { getAllHeroesResponse } from 'src/app/data/getAllHeroes';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
@@ -32,7 +33,7 @@ describe('HomePageComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
     component.offset = 0;
-    component.page = 2;
+    component.page = 200;
     component.hasMoreHeroes = true;
   }));
 
@@ -41,16 +42,7 @@ describe('HomePageComponent', () => {
   });
 
   it('should call getAllHeroes and return a list of heroes', () => {
-    const results =  [{
-      thumbnail: {
-        path: 'abc',
-        extension: 'png'
-      },
-    }];
-    const response = {
-      results,
-      total: 15
-    };
+    const response = getAllHeroesResponse.data;
 
     spyOn(marvelService, 'getAllHeroes').and.returnValue(of(response));
     component.getHeroes();
@@ -59,34 +51,20 @@ describe('HomePageComponent', () => {
 
     expect(component.hasMoreHeroes).toBeFalsy();
     expect(component.offset).toEqual(AppConfig.LIMIT);
-    expect(component.heroes).toEqual(results);
+    expect(component.heroes).toEqual(response.results);
     expect(component.isLoading).toBeFalsy();
   });
 
   it('should call getAllHeroes and concat to a list of heroes when heroes is avaiable', () => {
-    const results =  [{
-      thumbnail: {
-        path: 'result',
-        extension: 'png'
-      }
-    }];
-    const response = {
-      results
-    };
-    const currentHeroes = [{
-      thumbnail: {
-        path: 'abc',
-        extension: 'png'
-      }
-    }];
+    const response =  getAllHeroesResponse.data;
+    const currentHeroes = [];
     component.heroes = currentHeroes;
 
-    // component.heroes = [];
     spyOn(marvelService, 'getAllHeroes').and.returnValue(of(response));
     component.getHeroes();
 
     fixture.detectChanges();
-    expect(component.heroes).toEqual(currentHeroes.concat(results));
+    expect(component.heroes).toEqual(currentHeroes.concat(response.results));
   });
 
   it('should show loading when calling onscroll', () => {
